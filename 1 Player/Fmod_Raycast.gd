db1c954df06c7ba3a10eda372057917468ec3133
sycadelion <121386852+sycadelion@users.noreply.github.com>
2025-01-24 20:57:58 -0600

    Audio occlusion done, has no distance limit

--- 1 Player/Fmod_Raycast.gd
+++ 1 Player/Fmod_Raycast.gd
@@ -1,7 +1,6 @@
 extends FmodEventEmitter3D
 
 var OcclusionPercent: float = 33
-var paraValue = 0
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -10,20 +9,26 @@ func _ready() -> void:
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	if owner.AudioRay:
+	if owner.AudioRay and Engine.get_process_frames() % 5 == 0:
+		const MAX_COLLISIONS: int = 3
+		var entities: Array = []
+		var exclude = [owner,GameManager.PLAYER]
+		var paraValue = 0
 		var space_state = self.get_world_3d().direct_space_state
 		var origin = self.global_position
 		var listener = FmodServer.get_listener_transform3d(0)
 		var end = listener.origin
-		
-		var query_Ray = PhysicsRayQueryParameters3D.create(origin,end)
-		query_Ray.exclude = [owner,GameManager.PLAYER]
-		var result = space_state.intersect_ray(query_Ray)
-		var depth = float(result.size())
-		if result == {}:
-			self.set_parameter("Occlusion",0)
-			
-		else:
-			paraValue = (depth * OcclusionPercent) / 100
-			self.set_parameter("Occlusion",paraValue)
-			await get_tree().create_timer(.01).timeout
+
+		for i in MAX_COLLISIONS:
+			var query = PhysicsRayQueryParameters3D.create(origin,end)
+			query.set_exclude(exclude)
+			var result = space_state.intersect_ray(query)
+			if result:
+				entities.append(result.collider)
+				exclude.append(result.rid)
+				origin = result.position
+			else:
+				break
+
+		paraValue = (entities.size() * OcclusionPercent) / 100
+		self.set_parameter("Occlusion",paraValue)
