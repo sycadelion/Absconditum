ea83907afd246ee02a3966d80e83408fade91a81
sycadelion <121386852+sycadelion@users.noreply.github.com>
2025-02-26 04:49:33 -0600

    added sprint

--- 1 Player/States/PlayerRunState.gd
+++ 1 Player/States/PlayerRunState.gd
@@ -1,11 +1,26 @@
 class_name PlayerRunState extends PlayerMovementState
 
+@export var SPEED_MULTI: float = 3.0
+@export var ACCELERATION: float = 0.1
+@export var DECELERATION: float = 0.25
 
-# Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	pass # Replace with function body.
+@rpc("call_local")
+func Enter():
+	PLAYER.anim_tree["parameters/FallAndFloor/transition_request"] = "Move"
 
+func Update(delta: float):
+	PLAYER.update_gravity(delta)
+	PLAYER.update_input(PLAYER.SPEED * SPEED_MULTI,ACCELERATION,DECELERATION)
+	PLAYER.update_velocity()
+	
+	if Input.is_action_just_released("sprint"):
+		Transition.emit(self, "PlayerWalkState")
+	#if Input.is_action_just_pressed("sprint") and PLAYER.is_on_floor() and not GameManager.paused:
+		#Transition.emit("PlayerWalkState", "PlayerRunState")
+	#if Input.is_action_just_pressed("crouch") and PLAYER.is_on_floor() and not GameManager.paused:
+		#pass
+	if PLAYER.velocity.length() == 0.0 and PLAYER.is_on_floor() and not GameManager.paused:
+		Transition.emit(self, "PlayerIdleState")
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
+func Exit():
 	pass
--- 1 Player/States/PlayerWalkState.gd
+++ 1 Player/States/PlayerWalkState.gd
@@ -1,6 +1,5 @@
 class_name PlayerWalkState extends PlayerMovementState
 
-@export var SPEED: float = 5.0
 @export var ACCELERATION: float = 0.1
 @export var DECELERATION: float = 0.25
 
@@ -10,11 +9,11 @@ func Enter():
 
 func Update(delta: float):
 	PLAYER.update_gravity(delta)
-	PLAYER.update_input(SPEED,ACCELERATION,DECELERATION)
+	PLAYER.update_input(PLAYER.SPEED,ACCELERATION,DECELERATION)
 	PLAYER.update_velocity()
 	
-	#if Input.is_action_just_pressed("sprint") and PLAYER.is_on_floor() and not GameManager.paused:
-		#Transition.emit("PlayerWalkState", "PlayerRunState")
+	if Input.is_action_just_pressed("sprint") and PLAYER.is_on_floor() and not GameManager.paused:
+		Transition.emit(self, "PlayerRunState")
 	#if Input.is_action_just_pressed("crouch") and PLAYER.is_on_floor() and not GameManager.paused:
 		#pass
 	if PLAYER.velocity.length() == 0.0 and PLAYER.is_on_floor() and not GameManager.paused:
