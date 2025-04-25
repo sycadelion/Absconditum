ca1da90b62d8f004408f5d47eb4c5cd9531650b0
sycadelion <121386852+sycadelion@users.noreply.github.com>
2025-04-25 01:03:16 -0500

    basic input remapping(ugly)

--- 7 GUI/Menus/settings.gd
+++ 7 GUI/Menus/settings.gd
@@ -17,6 +17,31 @@ extends Control
 @onready var keybinds: PanelContainer = $GridContainer/Keybinds
 @onready var user: PanelContainer = $GridContainer/User
 
+#input setting things
+@onready var input_button_scene = preload("res://7 GUI/Menus/input_button.tscn")
+@onready var action_list = %ActionList
+var is_remapping = false
+var action_to_remap = null
+var remapping_button = null
+var input_actions = {
+	"up": "Forward",
+	"down": "Backward",
+	"left": "Left",
+	"right": "Right",
+	"jump": "Jump",
+	"shoot": "Primary Fire",
+	"altfire": "Alt. Fire",
+	"Skill1": "Use Ability",
+	"reload": "Reload",
+	"crouch": "Crouch",
+	"sprint": "Sprint",
+	"Weapon1": "Weapon 1",
+	"Weapon2": "Weapon 2",
+	"Inventory": "Inventory",
+	"quit": "Pause",
+	"FPS": "FPS Overlay"
+}
+
 var pause_updates: bool = false
 var active_menu = null
 var Text_Focused = false
@@ -48,6 +73,7 @@ func _ready() -> void:
 	graphics.hide()
 	keybinds.hide()
 	user.hide()
+	_create_action_list()
 	update_text()
 	_check_settings()
 
@@ -154,3 +180,56 @@ func _on_player_name_focus_exited() -> void:
 
 func _on_player_name_text_submitted(_new_text: String) -> void:
 	Text_Focused = false
+
+func _create_action_list():
+	InputMap.load_from_project_settings()
+	for item in action_list.get_children():
+		item.queue_free()
+	
+	for action in input_actions:
+		var button = input_button_scene.instantiate()
+		var action_label = button.find_child("LabelAction")
+		var input_label = button.find_child("LabelInput")
+		var events = InputMap.action_get_events(action)
+		
+		action_label.text = input_actions[action]
+		
+		if events.size() > 0:
+			input_label.text = events[0].as_text().trim_suffix(" (Physical)")
+		else:
+			input_label.text = ""
+		
+		action_list.add_child(button)
+		button.pressed.connect(_on_input_button_pressed.bind(button,action))
+
+func _on_input_button_pressed(button, action):
+	if !is_remapping:
+		is_remapping = true
+		action_to_remap = action
+		remapping_button = button
+		button.find_child("LabelInput").text = "Press key to bind..."
+
+func _input(event: InputEvent) -> void:
+	if is_remapping:
+		if (
+			event is InputEventKey || 
+			(event is InputEventMouseButton && event.pressed)
+		):
+			if event is InputEventMouseButton && event.double_click:
+				event.double_click = false
+			
+			InputMap.action_erase_events(action_to_remap)
+			InputMap.action_add_event(action_to_remap, event)
+			_update_action_list(remapping_button, event)
+			is_remapping = false
+			action_to_remap = null
+			remapping_button = null
+			
+			accept_event()
+
+func _update_action_list(button, event):
+	button.find_child("LabelInput").text = event.as_text().trim_suffix(" (Physical)")
+
+
+func _on_reset_button_pressed() -> void:
+	_create_action_list()
